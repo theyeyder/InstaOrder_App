d8c4a53fec5a0961a73ac89d8229e529cb22939b
theyeyder <eyderarroyo19@gmail.com>
2025-04-22 11:20:09 -0500

    Configuracion del JWT en core/settings.py

---
+++ InstaOrder/backend/core/settings.py
@@ -0,0 +1,18 @@
+# settings.py
+INSTALLED_APPS = [
+    ...,
+    'rest_framework',
+    'rest_framework_simplejwt',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
